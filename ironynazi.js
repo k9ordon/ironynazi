3912887e1500ada84e8b2a1a66919a1b0cdf80ae
kschreiber <klemens.schreiber@karriere.at>
2016-01-16 15:12:22 +0100

    added cloudinary upload

--- ironynazi.js
+++ ironynazi.js
@@ -1,23 +1,75 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+if (Meteor.isServer) {
+    Meteor.startup(function() {
+        if (!Meteor.settings.cloudinary.cloud_name) throw new Error('Missing config');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+        Cloudinary.config({
+            cloud_name: Meteor.settings.cloudinary.cloud_name,
+            api_key: Meteor.settings.cloudinary.api_key,
+            api_secret: Meteor.settings.cloudinary.api_secret
+        });
+    });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if (Meteor.isClient) {
+
+    Template.registerHelper('imgUrl', function(input) {
+        return Session.get('imgUrl');
+    });
+
+    Session.setDefault('imgUrl', false);
+    Session.setDefault('uploading', false);
+
+    Template.layout.helpers({
+        "uploading": function() {
+            Session.get('uploading');
+        }
+    });
+
+    Template.listImages.helpers({
+        "images": function() {
+            return Cloudinary.collection.find();
+        }
+    });
+
+    Template.createForm.events({
+        'submit form': function(e) {
+            e.preventDefault();
+
+            var file = document.querySelector('input[type=file]').files[0];
+            if (!file) return alert('u haz no image');
+
+            var text = document.querySelector('input[type=text]').value.trim();
+            if (!text) return alert('u haz no text');
+
+            console.log('uploading', text, file);
+
+            Session.set('uploading', true);
+
+            Cloudinary.upload([file], {
+                transformation: [{
+                    crop:"fill",
+                    width: 400,
+                    height: 600,
+                },
+                    {
+                    crop:"fit",
+                    width: 350,
+                    height: 550,
+                    overlay: "text:impact_50_bold_stroke:" + text,
+                    color: "white",
+                    border: "4px_solid_black"
+                }]
+            }, function(err, res) {
+                Session.set('uploading', false);
+
+                if(err) {
+                    alert('ARRRRRR! ' + err.error.message);
+                    return console.error(err);
+                }
+
+                console.log(res);
+                Session.set('imgUrl', res.url);
+            });
+        }
+    });
 }
